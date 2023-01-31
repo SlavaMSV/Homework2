f2e03ed8808a24e9041485e1c6a64a0ada822fa1
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-31 11:02:46 +0300

    Добавлена программа определения второй цифры трехзначного числа

---
+++ ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Программа принимает случайное трехзначное число
+и на выходе показывает вторую цифру числа 
+*/
+
+int getnumber ()
+{
+    Random rnd = new Random();
+    int result = rnd.Next(100, 1000);
+    return(result);
+}
+int getseconddigit (int number)
+{
+    int seconddigit = (number/10)%10;
+    return (seconddigit);
+}
+int number = getnumber();
+Console.WriteLine($"сгенерированное число: {number}");
+int digit2 = getseconddigit(number);
+Console.WriteLine ($"вторая цифра числа:{digit2}");
