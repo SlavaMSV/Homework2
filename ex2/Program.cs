204d22102f022a83f6f43aa5ea173c1ff2bf5399
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-31 16:36:21 +0300

    Добавили программу определения третьей цифры числа

---
+++ ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая выводит 
+третью цифру заданного числа или сообщает, 
+что третьей цифры нет */
+
+Console.WriteLine("Enter number:");      
+
+int getthirddigit (int number)
+{
+    int i = number;
+while (i / 100 > 9)
+i =  i / 10;
+return(i%10);
+}
+int number = Convert.ToInt32(Console.ReadLine());
+int digit3 = getthirddigit(number);
+if (number > 99)
+Console.WriteLine($"третья цифра числа:{digit3}");
+else
+Console.WriteLine("Нет третьей цифры");
+
+
+
+
+
+
