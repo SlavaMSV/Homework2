a27a31ee13254bc035ac5886021f45ccf042b962
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-31 19:11:55 +0300

    Добавлена программа определения рабочих и выходных дней по номеру дня недели

---
+++ ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным */
+string day (int daynumber)
+{
+    string result;
+    if ((daynumber == 6) || (daynumber == 7))
+    result = "выходной";
+    else if ((daynumber > 0) && (daynumber < 6))
+    result = "будний";
+    else 
+    result = "Введено некорректное значение";
+    return (result);
+}
+Console.WriteLine ("Введите число дня недели:");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+string weekday = day(daynumber);
+Console.WriteLine(weekday);
+
