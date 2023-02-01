baee2b9acd91efdf830f20380fa23e9b2be50442
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-01 10:33:14 +0300

    Добавил заглавные буквы в методы

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -2,21 +2,21 @@
 третью цифру заданного числа или сообщает, 
 что третьей цифры нет */
 
-Console.WriteLine("Enter number:");      
-
-int getthirddigit (int number)
+     
+int Getthirddigit (int number)
 {
     int i = number;
-while (i / 100 > 9)
-i =  i / 10;
-return(i%10);
+    while (i / 100 > 9)
+            i =  i / 10;
+    return(i%10);
 }
+Console.WriteLine("Enter number:");
 int number = Convert.ToInt32(Console.ReadLine());
-int digit3 = getthirddigit(number);
+int digit3 = Getthirddigit(number);
 if (number > 99)
-Console.WriteLine($"третья цифра числа:{digit3}");
+    Console.WriteLine($"третья цифра числа: {digit3}");
 else
-Console.WriteLine("Нет третьей цифры");
+    Console.WriteLine("Нет третьей цифры");
 
 
 
