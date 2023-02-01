63ea167041c7e1252eadd9f93b73252b9af89081
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-01 11:25:41 +0300

    Добавлены заглавные буквы в методы

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -3,18 +3,18 @@
 и на выходе показывает вторую цифру числа 
 */
 
-int getnumber ()
+int Getnumber ()
 {
     Random rnd = new Random();
     int result = rnd.Next(100, 1000);
     return(result);
 }
-int getseconddigit (int number)
+int Getseconddigit (int number)
 {
     int seconddigit = (number/10)%10;
     return (seconddigit);
 }
-int number = getnumber();
+int number = Getnumber();
 Console.WriteLine($"сгенерированное число: {number}");
-int digit2 = getseconddigit(number);
+int digit2 = Getseconddigit(number);
 Console.WriteLine ($"вторая цифра числа:{digit2}");
